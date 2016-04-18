d87f3c2e1fb71a49a657006b3365aedabae2a2e7
joshuabautista <joshuabautistar09@gmail.com>
2016-04-18 16:04:01 +0800

    First commit, models

---
+++ tracker/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Companie, Employee, Log
+
+admin.site.register(Companie)
+admin.site.register(Employee)
+admin.site.register(Log)
---
+++ tracker/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+
+class Companie(models.Model):
+	company = models.CharField(max_length=30)
+
+	def __str__(self):
+		return self.company
+
+class Employee(models.Model):
+	company = models.ForeignKey('Companie')
+	employee = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.employee
+
+class Log(models.Model):
+	employee = models.ForeignKey('Employee')
+	time_in = models.DateTimeField(
+            default=timezone.now)
+	time_out = models.DateTimeField(
+		blank=True, null=True)
+
+	def isave(self):
+		self.time_out = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.employee.employee
