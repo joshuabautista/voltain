0a7fadffa7aa6dd0eedbfb08189058314bf54a9a
joshuabautista <joshuabautistar09@gmail.com>
2016-04-19 13:18:45 +0800

    Modified templats to display employees and time ins from database

--- tracker/views.py
+++ tracker/views.py
@@ -5,5 +5,5 @@ from .models import Companie, Employee, Log
 def employee_list(request):
 	company = Companie.objects.all()
 	employees = Employee.objects.all()
-	log = Log.objects.filter(employee = employees)
+	log = Log.objects.all()
 	return render(request, 'tracker/employee_list.html', {'employees': employees, 'company' : company, 'log' : log})
